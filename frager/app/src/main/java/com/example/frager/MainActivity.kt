f29e5f548d3f74a94525fd893a3f39e8409f3e85
famedinap <famedinap@unal.edu.co>
2020-10-17 09:44:46 -0500

    vistas de login y registro

--- frager/app/src/main/java/com/example/frager/MainActivity.kt
+++ frager/app/src/main/java/com/example/frager/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.frager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text: TextView = findViewById(R.id.textView2) as TextView
+        text.setOnClickListener {
+            val intent:Intent = Intent (this, Registro ::class.java)
+            startActivity(intent)
+        }
+
     }
 }
